ebcf0bc546a52e9d3570e8840265f8a4579f74e0
Pedro Plowman <pedrofp@me.com>
2026-04-19 04:23:49 +1000

    test data

--- views/test/site/index.php
+++ views/test/site/index.php
@@ -24,44 +24,352 @@ $this->title = 'My Yii Application';
 
 $data = require dirname(__DIR__, 3) . '/data/p2-test-data.php';
 
-$bi = $data['bootstrapIcons'];
-$ei = $data['emojiIcons'];
-$fi = $data['flagIcons'];
+$bi = $data['Bootstrap'];
+$ei = $data['Emoji'];
+$fi = $data['Flag'];
 ?>
 <div class="site-index">
 	<div class="p-5 mb-4 bg-transparent rounded-3">
 		<div class="container-fluid py-5 text-center">
 			<h1 class="display-4">Test!</h1>
-			<p class="fs-5 fw-light">You have successfully created your Yii-powered application.</p>
-			<p><a class="btn btn-lg btn-success" href="https://www.yiiframework.com">Get started with Yii</a></p>
+			<p class="fs-5 fw-light">p2y2 test page</p>
 		</div>
 	</div>
 
-	<div class="body-content">
+	<div class="container body-content">
 
-		<div class="row" id="bi">
+		<div class="row" id="bi-circle-multipliers-0">
+			<div class="col-12">
+				<h2>Icons Circle Multipliers<br>
+				<small><code>BI::c(BI::i($name)->x($m))</code></small></h2>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(1)) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(2)) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(3)) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(4)) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(5)) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(6)) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(7)) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(8)) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(9)) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(10)) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+		</div>
+
+		<div class="row" id="bi-circle-multipliers-1">
+			<div class="col-12">
+				<h2>Icons Circle Multipliers<br>
+				<small><code>BI::c(BI::i($name)->x($m))</code></small></h2>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(1))->x(1) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(2))->x(2) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(3))->x(3) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(4))->x(4) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(5))->x(5) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(6))->x(6) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(7))->x(7) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(8))->x(8) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(9))->x(9) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::c(BI::i(BI::_SUBSTACK)->x(10))->x(10) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+		</div>
+
+		<div class="row" id="bi-s1-multiplied">
+			<div class="col-12">
+				<h2>Icons Multipliers<br>
+				<small><code>BI::i($name)->s(1)->x($m)</code></small></h2>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(1)->x(1) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(1)->x(2) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(1)->x(3) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(1)->x(4) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(1)->x(5) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(1)->x(6) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(1)->x(7) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(1)->x(8) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(1)->x(9) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(1)->x(10) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+		</div>
+
+		<div class="row" id="bi-multipliers">
+			<div class="col-12">
+				<h2>Icons Multipliers<br>
+				<small><code>BI::i($name)->x($m)</code></small></h2>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->x(1) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->x(2) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->x(3) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->x(4) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->x(5) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->x(6) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->x(7) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->x(8) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->x(9) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->x(10) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+		</div>
+
+		<div class="row" id="bi-sizes">
+			<div class="col-12">
+				<h2>Icons Sizes<br>
+				<small><code>BI::i($name)->s($s)</code></small></h2>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(1) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(2) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(3) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(4) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(5) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+			<div class="col text-center">
+				<?= BI::i(BI::_SUBSTACK)->s(6) ?>
+				<br><?= Html::encode(BI::_SUBSTACK) ?>
+			</div>
+		</div>
+
+		<div class="row" id="bi-icons">
+			<div class="col-12">
+				<h2>Bootstrap Icons<br>
+				<small><code>BI::i($name)</code></small></h2>
+			</div>
 			<?php foreach ($bi as $icon): ?>
-				<div class="col">
-					<?= BI::i($icon) ?><br>
-					<small><?= Html::encode($icon) ?></small>
+				<div class="col-2 text-center">
+					<span class="fs-1"><?= BI::i($icon) ?></span>
+					<br><?= Html::encode($icon) ?>
 				</div>
 			<?php endforeach; ?>
 		</div>
 
-		<div class="row" id="ei">
+		<div class="row" id="bi-icons-circles">
+			<div class="col-12">
+				<h2>Bootstrap Icons Circles<br>
+				<small><code>BI::c($icon)</code></small></h2>
+			</div>
+			<?php foreach ($bi as $icon): ?>
+				<div class="col-2 text-center">
+					<?= BI::c(BI::i($icon)->s(1)) ?><br>
+					<?= Html::encode($icon) ?>
+				</div>
+			<?php endforeach; ?>
+		</div>
+
+		<div class="row" id="bi-icons-squares">
+			<div class="col-12">
+				<h2>Bootstrap Icons Squares<br>
+				<small><code>BI::s($icon)</code></small></h2>
+			</div>
+			<?php foreach ($bi as $icon): ?>
+				<div class="col-2 text-center">
+					<?= BI::s(BI::i($icon)->s(1)) ?><br>
+					<?= Html::encode($icon) ?>
+				</div>
+			<?php endforeach; ?>
+		</div>
+
+		<div class="row" id="ei-icons">
+			<div class="col-12">
+				<h2>Emoji Icons<br>
+				<small><code>EI::i($name)</code></small></h2>
+			</div>
 			<?php foreach ($ei as $icon): ?>
-				<div class="col">
-					<?= EI::i($icon) ?><br>
-					<small><?= Html::encode($icon) ?></small>
+				<div class="col-2 text-center">
+					<span class="fs-1"><?= EI::i($icon) ?></span>
+					<br><?= Html::encode($icon) ?>
 				</div>
 			<?php endforeach; ?>
 		</div>
 
-		<div class="row" id="fi">
+		<div class="row" id="ei-icons-circles">
+			<div class="col-12">
+				<h2>Emoji Icons Circles<br>
+				<small><code>EI::c($icon)</code></small></h2>
+			</div>
+			<?php foreach ($ei as $icon): ?>
+				<div class="col-2 text-center">
+					<?= EI::c(EI::i($icon)->s(1)) ?><br>
+					<?= Html::encode($icon) ?>
+				</div>
+			<?php endforeach; ?>
+		</div>
+
+		<div class="row" id="ei-icons-squares">
+			<div class="col-12">
+				<h2>Emoji Icons Squares<br>
+				<small><code>EI::s($icon)</code></small></h2>
+			</div>
+			<?php foreach ($ei as $icon): ?>
+				<div class="col-2 text-center">
+					<?= EI::s(EI::i($icon)->s(1)) ?><br>
+					<?= Html::encode($icon) ?>
+				</div>
+			<?php endforeach; ?>
+		</div>
+
+		<div class="row" id="fi-icons">
+			<div class="col-12">
+				<h2>Flag Icons<br>
+				<small><code>FI::i($name)</code></small></h2>
+			</div>
 			<?php foreach ($fi as $icon): ?>
-				<div class="col">
-					<?= FI::i($icon) ?><br>
-					<small><?= Html::encode($icon) ?></small>
+				<div class="col-2 text-center">
+					<span class="fs-1"><?= FI::i($icon) ?></span>
+					<br><?= Html::encode($icon) ?>
+				</div>
+			<?php endforeach; ?>
+		</div>
+
+		<div class="row" id="fi-icons-circles">
+			<div class="col-12">
+				<h2>Flag Icons Circles<br>
+				<small><code>FI::c($icon)</code></small></h2>
+			</div>
+			<?php foreach ($fi as $icon): ?>
+				<div class="col-2 text-center">
+					<?= FI::c(FI::i($icon)->s(1)) ?><br>
+					<?= Html::encode($icon) ?>
+				</div>
+			<?php endforeach; ?>
+		</div>
+
+		<div class="row" id="fi-icons-squares">
+			<div class="col-12">
+				<h2>Flag Icons Squares<br>
+				<small><code>FI::s($icon)</code></small></h2>
+			</div>
+			<?php foreach ($fi as $icon): ?>
+				<div class="col-2 text-center">
+					<?= FI::s(FI::i($icon)->s(1)) ?><br>
+					<?= Html::encode($icon) ?>
 				</div>
 			<?php endforeach; ?>
 		</div>
